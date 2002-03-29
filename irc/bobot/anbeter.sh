fa9dae53d265ecdc6814b633942dc17ccffd1a33
dividuum <dividuum@bl0rg.net>
2002-03-29 12:02:19 +0000

    jor

--- irc/bobot/anbeter.sh
+++ irc/bobot/anbeter.sh
@@ -4,5 +4,5 @@
    sed -e "s/@IRCNAME@/$i/g" -e "s/@NICKNAME@/$i/g" anbeter/bot.conf.orig > anbeter/bot.$i.conf
    cd anbeter && bobot++ -f bot.$i.conf
    cd ..
-   sleep 20
+   sleep 3
 done) < anbeternamen.txt
